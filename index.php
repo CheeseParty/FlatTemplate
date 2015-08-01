6daaa633023929919281575dcff2feebb4319ef0
Gweedzy <pj.leborgne@gmail.com>
2015-08-01 22:21:15 +0200

    #Emmental

--- index.php
+++ index.php
@@ -2,29 +2,30 @@
 <!DOCTYPE HTML>
     <HTML>
         <head>
-            <title>HOME</title>
+            <title>Site - Accueil</title>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
             <link rel="stylesheet" href="style.css">
+            <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
             <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
             <link rel="icon" href="favicon.ico" type="image/x-icon">
         </head>
 
         <body>
             <div id="header">
-                <img src="img/logo.png" alt="logo">
+               <!-- <img src="img/logo.png" alt="logo">-->
                 <h1 class="headertitle">Nom du site</h1>
 
-                <ul>
+                <!--<ul>
                     <li><a class="currnavlink" href="index.php">1 Link</a></li>
                     <li><a class="navlink" href="index.php">2 Link</a></li>
                     <li><a class="navlink" href="index.php">3 Link</a></li>
                     <li><a class="navlink" href="index.php">4 Link</a></li>
-                </ul>
+                </ul>-->
             </div>
 
             <div id="container">
-
+                <h1 class="containertitle">Lorem Ipsum</h1>
             </div>
         </body>
 
