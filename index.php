8e1209ad82108502489797f27c740f1acce902ff
Gweedzy <pj.leborgne@gmail.com>
2015-08-01 23:11:41 +0200

    fgsdfg

--- index.php
+++ index.php
@@ -26,7 +26,10 @@
                     <li><a class="navlink" href="#">3 Link</a></li>
                     <li><a class="navlink" href="#">4 Link</a></li>
                 </ul>
+                <div id="fb-root"></div><script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class="fb-video" data-allowfullscreen="1" data-href="https://www.facebook.com/CodesMeufs/videos/1815173158696735/"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/CodesMeufs/videos/1815173158696735/"><a href="https://www.facebook.com/CodesMeufs/videos/1815173158696735/"></a><p>Meilleur vine au monde Mdddrrd&#039;autres videos aimez vite&gt;&gt;&gt; Codes de Meufs</p>Posted by <a href="https://www.facebook.com/CodesMeufs">Codes de Meufs</a> on dimanche 26 juillet 2015</blockquote></div></div>
+
             </div>
+
         </body>
 
     </HTML>
