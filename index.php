6cc112afc1fdf94dc3ef2538ecb481c131b9c695
Gweedzy <pj.leborgne@gmail.com>
2015-08-01 21:03:21 +0200

    First

--- index.php
+++ index.php
@@ -4,12 +4,20 @@
         <head>
             <title>HOME</title>
             <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
             <link rel="stylesheet" href="style.css">
         </head>
 
         <body>
             <div id="header">
+                <img src="img/logo.png" alt="logo">
 
+                <ul>
+                    <li><a href="index.php">1 Link</a></li>
+                    <li><a href="index.php">2 Link</a></li>
+                    <li><a href="index.php">3 Link</a></li>
+                    <li><a href="index.php">4 Link</a></li>
+                </ul>
             </div>
         </body>
 
