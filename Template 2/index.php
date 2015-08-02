dd8bb1b8e5cd76b81aec79d38f9d67038ff77a17
Gweedzy <pj.leborgne@gmail.com>
2015-08-02 15:44:52 +0200

    2nd Template

---
+++ Template 2/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Site - Accueil</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+        <div id="header">
+            <a class="sitename" href="index.php">Site Name</a>
+            <ul>
+                <li><a href="#" class="currnavlink">Accueil</a></li>
+                <li><a href="#" class="navlink">Mods</a></li>
+                <li><a href="#" class="navlink">Maps</a></li>
+                <li><a href="#" class="navlink">Forum</a></li>
+                <li><a href="#" class="navlink">Contact</a></li>
+            </ul>
+        </div>
+
+        <div id="container">
+            <p class="containertitle">Bienvenue sur notre site</p>
+                <p class="paragraphe">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus semper tempus. In tincidunt eget nisi maximus vehicula. Quisque sed pellentesque velit, quis vehicula leo. Suspendisse ultricies pellentesque molestie. Cras imperdiet ultricies ornare. Aliquam erat volutpat. Vivamus sed velit eget tellus consequat placerat nec quis metus. Aenean vel nunc tempus turpis consectetur porta. Nulla iaculis eros id rutrum ornare. Quisque consequat nunc leo, et vestibulum nulla pellentesque sed.</p>
+                <p class="paragraphe">Phasellus eget arcu odio. Quisque laoreet, nulla in imperdiet aliquet, ante justo consectetur enim, ut iaculis erat diam in magna. Integer nec interdum sem. Integer accumsan euismod eros, vel sodales lectus sagittis eget. Pellentesque eleifend lacus vel urna suscipit, nec varius massa tincidunt. Vivamus eleifend nec purus at placerat. Cras eget urna commodo dui placerat rhoncus et in nibh. Suspendisse ante ante, scelerisque vel placerat feugiat, efficitur at nisi. Pellentesque commodo non velit at tempus. Morbi sapien ante, pharetra at dictum ut, hendrerit vitae sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+                <p class="paragraphe">Ut velit mi, dapibus ut orci in, malesuada tempor nunc. Suspendisse ut justo in libero scelerisque mollis at id libero. Duis pulvinar nulla ac luctus ultricies. Nunc iaculis condimentum sem, ut tristique turpis posuere sed. Pellentesque eu lobortis purus. In vehicula dui in sapien sollicitudin, nec malesuada tortor efficitur. Vestibulum et libero dictum, varius diam non, ultrices sem. Vestibulum ac dapibus dolor. Suspendisse auctor lacus ut erat porta hendrerit. Fusce malesuada dui in tincidunt dignissim. Suspendisse condimentum enim magna, nec ultrices arcu sodales at. Fusce eros diam, gravida ac nibh vel, lobortis venenatis ipsum. Pellentesque gravida aliquam metus eu mollis. Cras euismod nisl at egestas laoreet.</p>
+            <p class="title">Deuxième titre</p>
+                <p class="paragraphe">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus semper tempus. In tincidunt eget nisi maximus vehicula. Quisque sed pellentesque velit, quis vehicula leo. Suspendisse ultricies pellentesque molestie. Cras imperdiet ultricies ornare. Aliquam erat volutpat.</p>
+                <p class="paragraphe">Phasellus eget arcu odio. Quisque laoreet, nulla in imperdiet aliquet, ante justo consectetur enim, ut iaculis erat diam in magna. Integer nec interdum sem. Integer accumsan euismod eros, vel sodales lectus sagittis eget. Pellentesque eleifend lacus vel urna suscipit, nec varius massa tincidunt. Vivamus eleifend nec purus at placerat. Cras eget urna commodo dui placerat rhoncus et in nibh. Suspendisse ante ante, scelerisque vel placerat feugiat, efficitur at nisi.</p>
+
+            <img src="https://upload.wikimedia.org/wikipedia/pt/7/71/Minecraft2_logo.png" alt="logominecraft">
+        </div>
+    </body>
+</html>
